cebd8408b2e662620548d68eee98b8dee5b09f1d
Kooreis <kooreis.official@gmail.com>
2023-09-30 21:01:24 -0500

    In this update, a while loop is added to keep the game running until the attempts run out. For each character in the word, it checks if the character has been guessed. If it has, it prints the character. If it hasn't, it prints an underscore and increments the 'failed' counter.

--- Text_Based_Hangman_Game.py
+++ Text_Based_Hangman_Game.py
@@ -1,10 +1,10 @@
-import random
+while attempts > 0:
 
-def hangman():
-    word_list = ['python', 'java', 'ruby', 'javascript']
-    random_number = random.randint(0, 3)
-    word = word_list[random_number]
-    guessed_letters = []
-    attempts = 10
+        failed = 0
 
-    print('Let's play Hangman!')
+        for char in word: 
+            if char in guessed_letters: 
+                print(char, end = '')
+            else: 
+                print('_', end = '')
+                failed += 1
